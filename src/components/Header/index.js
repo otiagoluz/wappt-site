e52c198a1266d1c40a019e9681809a520a4484cf
danisluz <danisluz@gmail.com>
2020-02-27 03:29:18 -0300

    add features

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,7 +13,7 @@ export default () => (
         <ul>
           <Link className="nav-link" to="/">PLANOS</Link>
           <Link className="nav-link" to="/">PORTFÓLIO</Link>
-          <Link className="nav-link" to="/">SOBRE</Link>
+          <Link className="nav-link" to="/">ABOUT</Link>
           <button className="nav-button" href="/">FALE CONOSCO</button>
         </ul>
       </nav>
