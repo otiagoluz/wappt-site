28d0fccf86a44a3b05bdf3840fdc5bcf46323000
danisluz <danisluz@gmail.com>
2020-02-28 02:58:16 -0300

    add new code home

---
+++ src/components/btncontrate/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.scss'
+
+import { Link } from 'gatsby'
+
+export default () => (
+    <Link to="/"><button className="btn-contrate">Contrate Agora</button></Link>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 import React from "react"
 import Header from '../components/Header/index'
+import Banner from '../components/banner/index'
+import Homedesign from '../components/homedesign/index'
+import Homegoogle from '../components/homegoogle/index'
+import Homeprocesso from '../components/homeprocesso/index'
 import '../assets/sass/global.scss'
 
 export default () => (
 
-<div>
-  <Header />
-</div>
+<>
+  <Header/>
+  <Banner/>
+  <Homedesign/>
+  <Homegoogle/>
+  <Homeprocesso/>
+</>
 
 )
